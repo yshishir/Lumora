d9472633760dcdedcac0a48af15e08f639ae2c7a
Shishir <shishirtech506@gmail.com>
2026-06-06 17:32:44 +0530

    prisma added

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,23 @@ app.post("/api/v1/user/signup", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/user/signup", (c) => {
+app.post("/api/v1/user/signin", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/user/signup", (c) => {
+app.post("/api/v1/blog", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/user/signup", (c) => {
+app.get("/api/v1/blog", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/user/signup", (c) => {
+app.put("/api/v1/blog", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/user/signup", (c) => {
+app.post("/api/v1/blog/bulk", (c) => {
   return c.text("Hello Hono!");
 });
 
