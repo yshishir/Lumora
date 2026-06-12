d17c6fdc78ea9769f5ef6236d8082e2175d3cb13
Shishir <shishirtech506@gmail.com>
2026-06-12 20:44:46 +0530

    protecting blog routes with jwt

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,28 @@
 import jwt from "jsonwebtoken";
 
-export function createToken(id: number) {
-  return jwt.sign({ id }, process.env.JWT_SECRET!, {
-    expiresIn: "7d",
-  });
+type TokenPayLoad = {
+  id: number;
+  email: string;
+  username: string;
+};
+
+export function getUserFromToken(req: Request) : TokenPayLoad | null {
+  const authHeader = req.headers.get("authorization");
+
+  if(!authHeader){
+    return null;
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  if(!token) {
+    return null;
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as TokenPayLoad;
+    return decoded;
+  } catch {
+    return null;
+  }
 }
