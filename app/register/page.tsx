e102ab2bd89a50dd123b2c7feb2875ad6a0bb835
Shishir <shishirtech506@gmail.com>
2026-06-14 19:08:51 +0530

    frontend: register page also done im going crazy

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,72 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="flex flex-1 flex-col">
+      <main className="flex flex-1 items-center justify-center px-4">
+        <Card className="w-full max-w-md">
+          <CardHeader>
+            <CardTitle className="text-xl">Create an account</CardTitle>
+            <CardDescription>
+              Enter your email below to create a new account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label>Username</Label>
+                  <Input
+                  id="name"
+                  type="name"
+                  placeholder="John Pork"
+                  required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="email-spacing">Email</Label>
+                  <Input
+                    id="email-spacing"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password-spacing">Password</Label>
+                  </div>
+                  <Input id="password-spacing" type="password" required />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className="flex-col gap-2">
+            <Button type="submit" className="w-full bg-[#2C3947] py-5 cursor-pointer hover:bg-[#2C3947]/90 text-lg">
+             Register
+            </Button>
+            <p className="text-center text-sm text-[#2C3947]/70">
+            Already have an account?{" "}
+              <Link href="/login" className="font-medium text-[#2C3947] underline-offset-2 hover:underline">
+                Login
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
+      </main>
+    </div>
+  );
 };
 
 export default Register;
